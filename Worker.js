734b4c8c698641e0d0bd0c636ad74c1444532654
ryu <oliver2wister@gmail.com>
2021-02-04 16:24:48 +0900

    web worker版お絵かきアプリ

--- Worker.js
+++ Worker.js
@@ -3,34 +3,74 @@
 //paper.setup([640, 480]);
 
 var offscreenCanvas;
-var ctx;
+var context;
 
 onmessage = event => {
     switch (event.data.type) {
         case 'init':
             init(event);
             break;
+        case 'pointerdown':
+            dragStart(event);
+            break;
+        case 'pointerup':
+            dragEnd(event);        
+            break;
         case 'pointermove':
             pointerMove(event);
             break;
+        
     }
 };
 
+const lastPosition = { x: null, y: null };
+let isDrag = false;
+let currentColor = '#000000';
+
+function dragStart(event) {
+    context.beginPath();
+
+    isDrag = true;
+  }
+
+  function dragEnd(event) {
+    context.closePath();
+    isDrag = false;
+    lastPosition.x = null;
+    lastPosition.y = null;
+  }
+
 function init(event) {
     offscreenCanvas = event.data.canvas;
-    ctx = offscreenCanvas.getContext('2d');
+    context = offscreenCanvas.getContext('2d');
     console.log('init');
 }
 
 function pointerMove(event) {
-    draw(event);
+    draw(event.data.x, event.data.y);
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function draw(event) {
-    ctx.fillRect(event.data.x, event.data.y, 3, 3);
+function draw(x, y) {
+    if(!isDrag) {
+      return;
+    }
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    context.lineWidth = 5;
+    context.strokeStyle = currentColor;
+    if (lastPosition.x === null || lastPosition.y === null) {
+      context.moveTo(x, y);
+    } else {
+      context.moveTo(lastPosition.x, lastPosition.y);
+    }
+    context.lineTo(x, y);
+    context.stroke();
+
+    lastPosition.x = x;
+    lastPosition.y = y;
 }
 
